0793f9a43cc4c74601fad2c08f13ee8cc831fed1
zerocool4545 <89600461+zerocool4545@users.noreply.github.com>
2021-09-13 21:35:23 +0545

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+
+    system("COLOR 0A");
+    printf("\n\n\n\n\n\n\n\t\t\t\t\tPROGRAM OF PERIODIC TABLE");
+    sleep(2);
+    system("cls");
+
+    for (i=0;i<=100;i++)
+    {
+           printf("\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\tLOADING %d%%",i);
+           Sleep(40);
+           system("cls");
+    }
+    sleep(2);
+    system("cls");
+
+}
